ceb14acbc882304df776c9c93875eaf638b8a7d8
Mateo Rodriguez <mateoytomas2009@hotmail.com>
2022-02-13 20:28:52 -0300

    Solucion ejercicios 5 y 6

---
+++ src/main/java/Ejercicio5y6.java
@@ -0,0 +1,25 @@
+public class Ejercicio5y6 {
+    public static void main(String[] args){
+        for (int i=0;i<=100;i++){
+            if((i%2) != 0){
+                System.out.println("Numero impar: "+i);
+            }else{
+                System.out.println("Numero par: "+i);
+            }
+        }
+        int x = 0;
+
+        /*
+        while(x<=100){
+            if((i%2) != 0){
+                System.out.println("Numero impar: "+i);
+            }else{
+                System.out.println("Numero par: "+i);
+            }
+            x++;
+        }
+
+         */
+
+    }
+}
